83371534ea41f3a1f48b0c4766e52c8f073fad7c
rigizer <rigizer@gmail.com>
2026-02-24 10:48:40 +0900

    수정) 안드로이드 최소 SDK 타겟을 33(Android 13)으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "org.duckdns.dorandoran.callaiassistant"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.duckdns.dorandoran.callaiassistant"
-        minSdk = 35
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
